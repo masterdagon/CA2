78dbccf618dc310bee2caabda70ae81bcc7cd3e8
masterdagon <masterdagon@hotmail.com>
2015-03-11 20:15:28 +0100

    trying to fix servlet.ini error. seems to be in CompanyResource

--- src/java/rest/ApplicationConfig.java
+++ src/java/rest/ApplicationConfig.java
@@ -30,7 +30,6 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(rest.AddressResource.class);
-//        resources.add(rest.CompanyResource.class);
         resources.add(rest.CompanyResource.class);
         resources.add(rest.PersonResource.class);
     }
