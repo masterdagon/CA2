d7eadc8612962498d142cb8f4febd59dec499de2
masterdagon <masterdagon@hotmail.com>
2015-03-11 13:10:23 +0100

    rest person

--- src/java/rest/PersonResource.java
+++ src/java/rest/PersonResource.java
@@ -5,9 +5,16 @@
  */
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import entity.Hobby;
+import entity.Person;
+import entity.Phone;
+import facade.Facade;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -24,29 +31,65 @@ public class PersonResource {
 
     @Context
     private UriInfo context;
-
-    /**
-     * Creates a new instance of PersonResource
-     */
+    private Facade f;
+    private Gson gson;
+    
     public PersonResource() {
+        f = new Facade();
+        gson = new Gson();
     }
 
-    /**
-     * Retrieves representation of an instance of rest.PersonResource
-     * @return an instance of java.lang.String
-     */
     @GET
     @Produces("application/json")
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    public String allProjects() {
+        List<Person> listP = f.getAllPersons();
+        JsonArray ja = new JsonArray();
+        for(Person p : listP){
+            JsonObject jo = new JsonObject();
+            jo.addProperty("id", p.getId());
+            jo.addProperty("firstname",p.getFirstName() );
+            jo.addProperty("lastname", p.getLastName());
+            jo.addProperty("email", p.getEmail());
+            
+            JsonObject address = new JsonObject();
+            address.addProperty("id", p.getAddress().getId());
+            address.addProperty("street", p.getAddress().getStreet());
+            address.addProperty("additionalinfo", p.getAddress().getAdditionalinfo());
+            
+            JsonObject city = new JsonObject();
+            city.addProperty("zipcode", p.getAddress().getCityInfo().getZipCode());
+            city.addProperty("city", p.getAddress().getCityInfo().getCity());
+            address.add("cityinfo", city);
+            jo.add("address",address);
+            
+            JsonArray phones = new JsonArray();
+            for(Phone ph : p.getPhones()){
+                JsonObject phone = new JsonObject();
+                phone.addProperty("number", ph.getNumber());
+                phone.addProperty("description",ph.getDescription());
+                phones.add(phone);
+            }
+            jo.add("phones", phones);
+            
+            JsonArray hobbies = new JsonArray();
+            for(Hobby h : p.getHobbies()){
+                JsonObject hobby = new JsonObject();
+                hobby.addProperty("id", h.getId());
+                hobby.addProperty("name", h.getName());
+                hobby.addProperty("description",h.getDescription());
+                hobbies.add(hobby);
+            }
+            jo.add("hobbies", hobbies);
+            
+            ja.add(jo);            
+        }
+        
+        String jsonString = gson.toJson(ja);
+        
+        return jsonString;
+        
     }
 
-    /**
-     * PUT method for updating or creating an instance of PersonResource
-     * @param content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
-     */
     @PUT
     @Consumes("application/json")
     public void putJson(String content) {
